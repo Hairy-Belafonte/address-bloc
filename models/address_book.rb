bffc454c66862e936d1d49c4ec89f21c04481688
Parris B <pjb7490@gmail.com>
2019-05-28 22:27:58 -0400

    searching assignment completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -46,5 +46,14 @@ class AddressBook
      # #5
      return nil
     end
+
+    def iterative_search(name)
+        @entries.each do |entry|
+            if entry.name == name
+                return entry
+            end
+        end
+        return nil
+    end
 end
 
